2d07c0f0bef889e5b03e401987c9250e0b41a21d
ali <mdali2016.227@gmail.com>
2022-08-06 00:24:55 +0600

    [Change] code refactored

--- src/app.js
+++ src/app.js
@@ -1,22 +1,5 @@
-import login from './js/login'
-import register from "./js/register";
+import pageLoader from "./js/page_loader";
 
 $('document').ready(() => {
-    $('#app').append(login.template)
-    $('#register_link').on('click', loadRegistrationPage)
-    login.script.loginButtonHandler()
-    login.script.checkAuth()
+    pageLoader.loadLoginPage()
 })
-
-function loadLoginPage (event) {
-    $('#app').empty()
-    $('#app').append(login.template)
-    login.script.loginButtonHandler()
-    $('#register_link').on('click', loadRegistrationPage)
-}
-function loadRegistrationPage (event) {
-    $('#app').empty()
-    $('#app').append(register.template)
-    register.script.registerButtonHandler()
-    $('#login_link').on('click', loadLoginPage)
-}
